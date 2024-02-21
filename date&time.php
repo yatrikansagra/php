178054a2ccea70c507bc330c79263a06e437a34d
yatrikanasagara <155125004+yatrikansagra@users.noreply.github.com>
2024-02-21 11:22:36 +0530

    Add files via upload

---
+++ date&time.php
@@ -0,0 +1,23 @@
+<?php
+date_default_timezone_set("Asia/kolkata");
+
+
+/*echo "<br>";
+echo mktime("HH:MM:SS");
+$a=date("d-m-y");
+echo $a;
+*/
+
+
+//$b=time();
+//echo $b;
+
+//$i= date("d-m-y");
+//echo $i;
+echo "<br>";
+date_default_timezone_set("Asia/kolkata");
+echo date("H:i:s");
+
+
+
+?>
